050a74f3dab01a44d9f019f216f13c6a2022d1fa
Jakub Opiełka <jakub1.opielka1@interia.pl>
2023-05-01 14:10:14 +0200

    namespaces correcting

--- QuizGenerator.Database/QuizGeneratorDbContext.cs
+++ QuizGenerator.Database/QuizGeneratorDbContext.cs
@@ -1,12 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using QuizGenerator.Database.Entities;
 
 namespace QuizGenerator.Database
 {
-    class QuizGeneratorDbContext
+    public class QuizGeneratorDbContext : DbContext
     {
+
+        public DbSet<Question> Questions { get; set; }
+
+        public DbSet<Quiz> Quizzes { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+
+            optionsBuilder.UseSqlite($"Filename={Path.Combine(System.Environment.GetFolderPath(System.Environment.SpecialFolder.MyDocuments), "quizGen.sqlite")}");
+        }
+
     }
 }
--- QuizGenerator/Pages/QuizGenFormPage.xaml.cs
+++ QuizGenerator/Pages/QuizGenFormPage.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace QuizGenerator.Pages
+namespace QuizGenerator
 {
     /// <summary>
     /// Interaction logic for QuizGenFormPage.xaml
