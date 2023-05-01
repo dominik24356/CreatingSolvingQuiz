22fa4d2de0dddca986a83f9ed06409d740c8b2de
Jakub Opiełka <jakub1.opielka1@interia.pl>
2023-05-01 12:54:50 +0200

    Creating proper QuizGen Projects with dependencies

---
+++ QuizGenerator.Database/Entities/Question.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizGenerator.Database.Entities
+{
+    class Question
+    {
+        public int Id { get; set; }
+
+        public string OptionA { get; set; }
+
+        public string OptionB { get; set; }
+
+        public string OptionC { get; set; }
+
+        public string OptionD { get; set; }
+
+        public string CorrectOption { get; set; }
+
+        [ForeignKey("Quiz")]
+        public int QuizId { get; set; }
+
+        public Quiz Quiz { get; set; }
+    }
+}
