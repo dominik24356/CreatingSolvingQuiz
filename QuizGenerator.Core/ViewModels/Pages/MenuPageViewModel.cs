795fa4d1cfb7e0ddf06223215f21acf74697eeff
Dominik Szczepanik <dominik16333@gmail.com>
2023-05-05 02:09:41 +0200

    adding pages for navigation

---
+++ QuizGenerator.Core/ViewModels/Pages/MenuPageViewModel.cs
@@ -0,0 +1,32 @@
+﻿using QuizGenerator.Core.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace QuizGenerator.Core.ViewModels.Pages
+{
+    public class MenuPageViewModel
+    {
+        public ICommand startGeneratingCommand { get; set; }
+        public ICommand startSolvingCommand { get; set; }
+
+        public MenuPageViewModel()
+        {
+            startGeneratingCommand = new RelayCommand(startGenerating);
+            startSolvingCommand = new RelayCommand(startSolving);
+        }
+
+
+        public void startGenerating() 
+        {
+            
+        }
+
+        public void startSolving()
+        { 
+        }
+    }
+}
